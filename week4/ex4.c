d65a8179c4cd35402eb788d00da73bac2937d7f3
Андрей Чертков <andreychertckov@yandex.ru>
2018-09-12 22:50:28 +0300

    lab4

--- week4/ex4.c
+++ week4/ex4.c
@@ -1,45 +1,39 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <string.h>
 
-char** split(char * string, char c) {
-	char *result[20];
-	char temp;
-	int start = 0;
-	int array_index= 0;
-	for(int i = 0; string[i] != '\0'; i++) {
-		if(temp == c) {
-			result[array_index] = malloc(256);
-			strncpy(result[array_index], string+start, i-start);
-			start = i+1;
-		}
+
+int main(void){
+  char *argv[40];
+  char user_input[80]; 
+  int i = 0;
+  while(1){
+    printf("$ ");
+    scanf ("%[^\n]%*c", user_input);
+
+    i = 0;
+    argv[i] = strtok(user_input," ");
+    while (argv[i] != NULL){
+      i++;
+      argv[i] = strtok(NULL, " ");
+    }
+
+    if(strcmp(argv[0], "exit") == 0) /* exit command */
+      break;
+
+    pid_t pid; 
+    pid = fork(); /* fork child process */
+    if(pid < 0){  //if fork failed due to some reason 
+        return 1;
+    }
+    
+    if (pid == 0){
+        execvp(argv[0],argv); /* execute the command  */
+    }else {
+		wait(NULL);
 	}
-	return result;
-}
-
-void main() {
-	while(1) {
-		char * input= malloc(256);
-		printf("$ ");
-		scanf("%s", input);
-
-		pid_t pid;
-		switch(pid = fork()) {
-			case -1:
-				printf("error");
-				exit(1);
-				break;
-			case 0: {
-					printf("test");
-					char ** commands = split(input, ' ');
-					char * command = commands[0];
-					char ** argv = malloc(1024);
-					printf("%d \n", command);
-					memcpy(argv, commands+1, 1024);
-					execv(commands[0],argv, NULL);
-					break;
-				}
-			default:
-				printf("test");
-		}
-	}
+  }
+  return 0;
 }
